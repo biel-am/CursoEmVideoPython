4a1964c4ed6e45d21025ed2cef23aa8917a076ee
Gabriel <gabialexandremedeiros@gmail.com>
2025-02-15 13:10:47 -0300

    commit exercícios

---
+++ ExerciciosPython/Ex018.py
@@ -0,0 +1,5 @@
+from math import sin, cos, tan
+ang = int(input('Digite o valor de um ângulo: '))
+print(f'Seu seno é {sin(ang):.2f}')
+print(f'Seu cosseno é {cos(ang):.2f}')
+print(f'Sua tangente é {tan(ang):.2f}')
